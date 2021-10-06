d98947af23bd25f0f2e0c1d0a394548751d62fef
JasonHangZhou <jhfnetboy@gmail.com>
2021-10-06 12:03:30 +0800

    wallet base build ok,to be test

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,44 +1,85 @@
-// import logo from './logo.svg';
 import './App.css';
-// import { Suspense, useEffect, useRef, useState } from 'react'
-// import { useLocation } from 'react-router-dom'
-// import { AppBar } from '../components'
-// import Web3ReactManager from '../components/Web3ReactManager'
-// import Routes from './routes'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDog } from '@fortawesome/free-solid-svg-icons'
-import {useEffect, useState} from "react";
-// import Link from 'next / link';
-// import wallet_model from '../src/models/wallet_model';//models/wallet_model.ts
+
+import { providers } from 'ethers';
+// import { Provider } from '@ethersproject/providers'
+import React, { useEffect, useRef, useState } from 'react';
+import useGreeter from './hooks/useGreeter';
+import useNetwork from './hooks/useNetwork';
 
 // function App(): JSX.Element {
 function App(){  
+      // wallet connect
+      const [network, setNetwork] = useState<providers.Network>()
+      const [account, setAccount] = useState<string>()
+      const [greet, setGreet] = useState<string>()
+      const [appMsg, setAppMsg] = useState<string>()
+      const inputElm = useRef<HTMLInputElement>(document.createElement("input"))
 
-  //   const {web3Loading, getweb3} = wallet_model ();
-  //   const [myWeb3, setMyWeb3] = useState ();
+      const [{ web3 }, handleNetwork] = useNetwork()
+      const [fetchGreeting, setGreeting] = useGreeter({ web3 })
 
-  // async function connectWallet () {
-  //   // ​    await getweb3().then((response) => {
-  //   //   ​		setMyWeb3 (response);
-  //     const web3con = await getweb3()
-  //     setMyWeb3 (response)
-  //     // ​   response.eth.getAccounts().then((result) => (
-  //     //       console.log (result)
-  //     //     ))      
-  //     // })  
-  //   }
+      useEffect(() => {
+        if (typeof web3 === "undefined") {
+          initFields()
+        } else {
+          const setNetworkAccount = async () => {
+            web3.detectNetwork().then(setNetwork)
+              .catch(showAppMsg)
+            web3.listAccounts().then(accounts => {
+              setAccount(accounts[0])
+            }).catch(showAppMsg)
+          }
+          setNetworkAccount()
+        }
+      }, [web3])
 
+      const initFields = () => {
+        setNetwork(undefined)
+        setAccount(undefined)
+        setGreet(undefined)
+      }
+
+      const showAppMsg = (err: any) => {
+        console.log(err)
+        setAppMsg(err.message || `${err}`)
+        setTimeout(() => setAppMsg(undefined), 7000)
+      }
+
+      const handleConnect = async () => {
+        handleNetwork().catch(showAppMsg)
+      }
+
+      const handleFetch = async () => {
+        fetchGreeting().then(setGreet).catch(showAppMsg)
+      }
+
+      const handleSet = () => {
+        setGreeting(inputElm.current.value)
+          .then(() => {
+            inputElm.current.value = ""
+            handleFetch()
+          })
+          .catch(showAppMsg)
+      }
+      //end wallet func
+
+    //begin mint func
     const address = "0x0bE9d4386f58022c27B0Fb035c46de7c5800938F" //should input from page
     const walkerName = "testWalkerName" //should input from page
-
+    
     async function mintWalker() {
       // if (!chainId) return
       const walker_json = require('../src/') ///src/artifacts/contracts/Walker.sol/Walker.json
       console.log(walker_json)
       // const provider = hre.ethers.getDefaultProvider(config['network'])
       // console.log(provider)
-    }
-    const userState = null
+    } 
+    //end mint func
+
+    
+    //begin scroll function
     let end = false
     let i = 0
     const txtList = ["My dog has been robbed by Cerberus？", "Oh my poor doggy!  I must save him in this weekend!", "So you carring on your umbrella and dog leash, down to the cellar.", "A adventure is beginging..."]
@@ -62,6 +103,9 @@ function App(){
       }
 
     }
+    // end scroll func
+
+    // return result
     return (
             <div className="flex flex-col h-screen items-start overflow-x-hidden bg-custom-background">
                 <header className="App-header">
@@ -81,12 +125,23 @@ function App(){
                  
                 </div>
                 </div>
-                {/* <div>
-                {web3Loading? <button className =" btn-inner - text "disabled> Loading ... </button>: <button className =" btn-inner - text "onClick = {connectWallet}>get accounts</button>}
-                </div> */}
+                <div>
+                  <h5>Hardhat Ethers Greeter Dapp Demo</h5>
+                  <button onClick={handleConnect}>{web3 ? "Disconnect" : "Connect"}</button><br />
+                  <button onClick={handleFetch}>Fetch Greeting</button><br />
+                  <button onClick={handleSet}>Set Greeting</button>
+                  <input ref={inputElm} placeholder="Set greeting" /><br />
+                  <hr />
+                  Network: {network?.chainId} {network?.name}<br />
+                  Account: {account}<br />
+                  Greet: {greet}<br />
+                  StatusMessage: {appMsg}<br />
+                  <hr />
+                  <i>Note: Please try Ropsten testnet by MetaMask.</i>
+                </div>
                 </header> 
             </div>
-    )
-}
+    )//end return
+}//end app func
 
 export default App
